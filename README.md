0ea5d7147e0aad420d5dcf5d67dd3fd28baa772a
Ozzy <cfhamlet@gmail.com>
2018-12-24 11:43:27 +0800

    add: init commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# os-sanic
+
+[![Build Status](https://www.travis-ci.org/cfhamlet/os-sanic.svg?branch=master)](https://www.travis-ci.org/cfhamlet/os-sanic)
+[![codecov](https://codecov.io/gh/cfhamlet/os-sanic/branch/master/graph/badge.svg)](https://codecov.io/gh/cfhamlet/os-sanic)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/os-sanic.svg)](https://pypi.python.org/pypi/os-sanic)
+[![PyPI](https://img.shields.io/pypi/v/os-sanic.svg)](https://pypi.python.org/pypi/os-sanic)
+
+A framework to organize [Sanic](https://github.com/huge-success/sanic) project and simplify development.
+
+
+
+# Install
+
+  ```
+  $ pip install os-sanic
+  ```
+
+
+# Unit Tests
+
+  ```
+  $ tox
+  ```
+
+# License
+
+MIT licensed.
---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+
+def read(*filenames, **kwargs):
+    import io
+    from os.path import join, dirname
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(join(dirname(__file__), filename), encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+
+setup(
+    name='os-sanic',
+    version=read('src/os_sanic/VERSION'),
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    license='MIT License',
+    description='A framework to simplify Sanic daemon development.',
+    long_description=open('README.md').read(),
+    author='Ozzy',
+    author_email='cfhamlet@gmail.com',
+    url='https://github.com/cfhamlet/os-sanic',
+    install_requires=open('requirements.txt').read().split('\n'),
+    python_requires='>=3.6',
+    zip_safe=False,
+    entry_points={
+        'console_scripts': ['os-sanic = os_sanic.cmdline:execute']
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ])
