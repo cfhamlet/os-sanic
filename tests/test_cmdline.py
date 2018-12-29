fc3c2ff9bf6d4366f90b9f6f620d4098d73d52be
Ozzy <cfhamlet@gmail.com>
2018-12-29 19:10:17 +0800

    test: test info

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -30,6 +30,31 @@ def test_startproject_001(tmpdir):
     ]
     with cd(tmpdir):
         print(tmpdir)
-        stdout, stderr = call(runf, 'startproject xxx', env)
+        call(runf, 'startproject xxx', env)
         for f in files:
             assert os.path.exists(os.path.join(tmpdir.strpath, f))
+
+
+def create_project(tmpdir, proj_name):
+    f = 'cmd_runner.py'
+    runf = os.path.join(tmpdir.strpath, f)
+    copy_file(f, runf)
+    env = os.environ.copy()
+    env['COVERAGE_PROCESS_START'] = os.path.abspath('.coveragerc')
+    env['COVERAGE_FILE'] = os.path.abspath('.coverage')
+
+    with cd(tmpdir):
+        call(runf, 'startproject {}'.format(proj_name), env)
+
+
+def test_info(tmpdir):
+    proj_name = 'xxx'
+    create_project(tmpdir, proj_name)
+    proj_path = os.path.join(tmpdir.strpath, proj_name)
+    env = os.environ.copy()
+    env['COVERAGE_PROCESS_START'] = os.path.abspath('.coveragerc')
+    env['COVERAGE_FILE'] = os.path.abspath('.coverage')
+    with cd(proj_path):
+        print(proj_path)
+        copy_file('manage_for_test.py', 'manage.py')
+        call('manage.py', 'info', env)
